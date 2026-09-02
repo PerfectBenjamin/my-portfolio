dd8cf341332ffb6aa4fed9490719edf038096da6
Perfect Benjamin <perfectmaji98@gmail.com>
2026-09-02 11:41:22 +0100

    Refactor code structure for improved readability and maintainability

--- pages/archive.js
+++ pages/archive.js
@@ -85,6 +85,52 @@ export default function Archive() {
                     2026
                   </td>
                   <td className="py-4 pr-8 align-top text-sm font-medium text-slate-200">
+                    SendVoy Website
+                  </td>
+                  <td className="hidden py-4 pr-8 align-top text-sm text-slate-400 lg:table-cell">
+                    SendVoy Limited
+                  </td>
+                  <td className="hidden py-4 pr-8 align-top lg:table-cell">
+                    <ul className="flex flex-wrap gap-2">
+                      {["HTML", "CSS", "JavaScript"].map((t) => (
+                        <li key={t}>
+                          <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                            {t}
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td className="hidden py-4 align-top text-sm sm:table-cell">
+                    <a
+                      href="https://sendvoy.com/"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      className="inline-flex items-baseline gap-1 font-medium leading-tight text-slate-400 hover:text-teal-300 focus-visible:text-teal-300 text-sm"
+                    >
+                      sendvoy.com
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="inline-block h-4 w-4 shrink-0 translate-y-px"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr className="border-b border-slate-300/10 last:border-none">
+                  <td className="py-4 pr-8 align-top text-sm text-slate-500">
+                    2026
+                  </td>
+                  <td className="py-4 pr-8 align-top text-sm font-medium text-slate-200">
                     Zairah Creatives
                   </td>
                   <td className="hidden py-4 pr-8 align-top text-sm text-slate-400 lg:table-cell">
